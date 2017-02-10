4e6d40c467881871e770c6fdfc03b1d6796d8eb9
stephanlindauer <stephanlindauer@posteo.de>
2017-02-11 00:59:59 +0100

    more logging

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:3.5
 
 RUN apk add --update bash docker
 
-COPY cleanup.sh /bin/clean-up.sh
+COPY clean-up.sh /bin/clean-up.sh
 RUN chmod +x /bin/clean-up.sh
 
 CMD echo "$CRONTAB /bin/clean-up.sh" > /var/spool/cron/crontabs/root && crond -f
--- clean-up.sh
+++ clean-up.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+echo "Starting at $(date)"
+
+# Containers
+echo "Containers before:"
+docker ps -a
 echo "Removing stopped containers:"
 docker rm $(docker ps --filter status=exited -q)
+echo "Containers after:"
+docker ps -a
 
+# Images
+echo "Images before:"
+docker images -a
 echo "Removing unused images"
 docker rmi $(docker images -q)
+echo "Images after:"
+docker images -a
 
+# Volumes
+echo "Volumes before:"
+docker images -a
 echo "Removing dangling volumes"
 docker volume rm $(docker volume ls -f dangling=true -q)
+echo "Volumes after:"
+docker images -a
